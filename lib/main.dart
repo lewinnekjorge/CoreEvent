01d33cf5a52eb791c23512a8d45cf298ba4ee621
Ricardo Gomez <ricardo_a_gomez@hotmail.com>
2021-11-30 16:55:02 -0500

    Update Login, Register, validacion de campos, creacion de una pagina principal para qu se unaan los otros witdgets, boton logout y boton tema, todo usando getx

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'ui/app.dart';
+import 'ui/pages/content_start.dart';
 
 void main() {
+  Get.lazyPut<InicioWidget>(() => const InicioWidget(title: 'main'));
   runApp(const App());
 }
